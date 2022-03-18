1e402c2666de51bc453a443bed9fb2cdf9722c54
Ivan987456 <Ivanstrelok18@gmail.com>
2022-03-18 12:19:36 +0300

    Добавьте файлы проекта.

---
+++ CBT3/Program.cs
@@ -0,0 +1,8 @@
+﻿int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int i;
+for (i = 0; i < a; i++)
+{
+    a -= b;
+}
+Console.WriteLine("Частное: " + i + "\nОстаток:" + a);
